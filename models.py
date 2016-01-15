c6fd57c889219d7e43514107de5a2cebddcada30
Greg Quinlan <quinlangl@gmail.com>
2016-01-14 20:34:06 -0500

    feat: Introduce KeyProperty fields, specfify kinds, computed conferenceKey for Sessions, reorder models, remove conferenceId for Sessions

--- models.py
+++ models.py
@@ -23,29 +23,6 @@ class ConflictException(endpoints.ServiceException):
     http_status = httplib.CONFLICT
 
 
-class Profile(ndb.Model):
-    """Profile -- User profile object"""
-    displayName = ndb.StringProperty()
-    mainEmail = ndb.StringProperty()
-    teeShirtSize = ndb.StringProperty(default='NOT_SPECIFIED')
-    conferenceKeysToAttend = ndb.StringProperty(repeated=True)
-    sessionWishlistKeys = ndb.StringProperty(repeated=True)
-
-
-class ProfileMiniForm(messages.Message):
-    """ProfileMiniForm -- update Profile form message"""
-    displayName = messages.StringField(1)
-    teeShirtSize = messages.EnumField('TeeShirtSize', 2)
-
-
-class ProfileForm(messages.Message):
-    """ProfileForm -- Profile outbound form message"""
-    displayName = messages.StringField(1)
-    mainEmail = messages.StringField(2)
-    teeShirtSize = messages.EnumField('TeeShirtSize', 3)
-    conferenceKeysToAttend = messages.StringField(4, repeated=True)
-
-
 class StringMessage(messages.Message):
     """StringMessage-- outbound (single) string message"""
     data = messages.StringField(1, required=True)
@@ -85,10 +62,31 @@ class ConferenceForm(messages.Message):
     websafeKey = messages.StringField(11)
     organizerDisplayName = messages.StringField(12)
 
-
+    
 class ConferenceForms(messages.Message):
     """ConferenceForms -- multiple Conference outbound form message"""
     items = messages.MessageField(ConferenceForm, 1, repeated=True)
+
+
+class Speaker(ndb.Model):
+    """Speaker -- Speaker profile object"""
+    displayName = ndb.StringProperty(required=True)
+    mainEmail = ndb.StringProperty()
+    bio = ndb.StringProperty()
+
+
+class SpeakerForm(messages.Message):
+    """SpeakerForm -- Speaker outbound form message"""
+    displayName = messages.StringField(1)
+    mainEmail = messages.StringField(2)
+    bio = messages.StringField(3)
+    creatorUserId = messages.StringField(4)
+    websafeKey = messages.StringField(5)
+
+
+class SpeakerForms(messages.Message):
+    """SpeakerForms -- multiple Speaker outbound form message"""
+    items = messages.MessageField(SpeakerForm, 1, repeated=True)
 
 
 class TeeShirtSize(messages.Enum):
@@ -127,35 +125,27 @@ class Session(ndb.Model):
     """Session -- Session object"""
     name = ndb.StringProperty(required=True)
     highlights = ndb.StringProperty()
-    speaker = ndb.StringProperty()
+    speakerKey = ndb.KeyProperty(Speaker)
     typeOfSession = ndb.StringProperty()
     duration = ndb.IntegerProperty()
-    date = ndb.DateProperty()
-    startTime = ndb.TimeProperty()
-    conferenceId = ndb.StringProperty()
+    date = ndb.DateProperty(required=True)
+    startTime = ndb.TimeProperty(required=True)
+    conferenceKey = ndb.ComputedProperty( lambda self: self.key.parent() )
 
 
 class SessionForm(messages.Message):
-    """SessionForm -- Session outbound form message
-    In the SessionForm pass in:
-    Session name
-    highlights
-    speaker
-    duration
-    typeOfSession
-    date
-    start time (in 24 hour notation so it can be ordered).
-    Ideally, create the session as a child of the conference."""
+    """SessionForm -- Session outbound form message"""
     name = messages.StringField(1)
     highlights = messages.StringField(2)
-    speaker = messages.StringField(3)
+    speakerName = messages.StringField(3)
     typeOfSession = messages.StringField(4)
     duration = messages.IntegerField(5)
     date = messages.StringField(6)
     startTime = messages.StringField(7)
-    conferenceId = messages.StringField(8)
     websafeKey = messages.StringField(9)
     conferenceName = messages.StringField(10)
+    speakerKey = messages.StringField(11)
+
 
 
 class SessionForms(messages.Message):
@@ -163,22 +153,24 @@ class SessionForms(messages.Message):
     items = messages.MessageField(SessionForm, 1, repeated=True)
 
 
-class Speaker(ndb.Model):
-    """Speaker -- Speaker profile object"""
+class Profile(ndb.Model):
+    """Profile -- User profile object"""
     displayName = ndb.StringProperty()
     mainEmail = ndb.StringProperty()
-    bio = ndb.StringProperty()
-    creatorUserId = ndb.StringProperty()
+    teeShirtSize = ndb.StringProperty(default='NOT_SPECIFIED')
+    conferenceKeysToAttend = ndb.KeyProperty(Conference, repeated=True)
+    sessionWishlistKeys = ndb.KeyProperty(Session, repeated=True)
 
 
-class SpeakerForm(messages.Message):
-    """SpeakerForm -- Speaker outbound form message"""
+class ProfileMiniForm(messages.Message):
+    """ProfileMiniForm -- update Profile form message"""
+    displayName = messages.StringField(1)
+    teeShirtSize = messages.EnumField('TeeShirtSize', 2)
+
+
+class ProfileForm(messages.Message):
+    """ProfileForm -- Profile outbound form message"""
     displayName = messages.StringField(1)
     mainEmail = messages.StringField(2)
-    bio = messages.StringField(3)
-    creatorUserId = messages.StringField(4)
-
-
-class SpeakerForms(messages.Message):
-    """SpeakerForms -- multiple Speaker outbound form message"""
-    items = messages.MessageField(SpeakerForm, 1, repeated=True)
+    teeShirtSize = messages.EnumField('TeeShirtSize', 3)
+    conferenceKeysToAttend = messages.StringField(4, repeated=True)
